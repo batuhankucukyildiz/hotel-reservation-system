47006f48db479b64bda107867fc08fb65fa54aef
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2023-06-20 23:58:54 +0300

    ping add

---
+++ server/controller/ping.js
@@ -0,0 +1,9 @@
+
+
+const ping = (request , response ) => {
+
+    response.status(200).json("pong")
+}
+
+
+export default ping;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,12 @@
 import express from "express" 
 import loginUser from "../controller/userController.js";
-
+import ping from "../controller/ping.js"
 
 const route = express() 
 
 
+
+route.get("/ping" , ping)
 route.get("/login" , loginUser) 
 
 
